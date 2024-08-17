8d505f6c40b322a669c62faf75fd558ee4ee0e31
blue262 <itskalyan.262@gmail.com>
2024-08-17 12:11:11 +0530

    user controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken")
+module.exports.register = async (req, res) => {
+    try {
+        const { name, profilePic, password, email, friends } = req.body;
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            name,
+            profilePic,
+            password: passwordHash,
+            email,
+            friends
+        });
+        const savedUser = await newUser.save();
+
+        res.status(201).json(savedUser);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+};
+
+module.exports.login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) return res.status(400).json({ msg: "User does not exist. " });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+  
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      delete user.password;
+      res.status(200).json({ token, user });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
